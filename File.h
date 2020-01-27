c345e9d846abaf0ff0e38fd18d6b864cab6f9f09
Nicholas Barnes <nicholas@nsb-electronics.co.za>
2020-01-27 14:45:50 +0200

    Add upload function
    Refactor code

--- File.h
+++ File.h
@@ -3,36 +3,34 @@
 
 #include "NSB_UC20.h"
 
-
-
-class UC_FILE
-{
+class UC_FILE {
 public:
 	UC_FILE();
 	void begin();
 	long GetSpace(String pattern);
 	long GetFreeSpace(String pattern);
 	void List(String pattern);
-	long List(String pattern,String fn);
+	long List(String pattern, String filename);
 	bool available();
 	String ReadLine();
-	bool Delete(String pattern,String fn);
-	int Open(String pattern,String fn);
+	bool Delete(String pattern, String filename);
+	int Open(String pattern, String filename);
 	bool Close(int handle);
 	bool Close_(int handle);
-	bool BeginWrite(int handle,int size);
+	bool BeginWrite(int handle, int size);
 	void Write(char data);
 	void Print(String data);
 	void Println(String data);
 	bool WaitFinish();
-	bool Seek(int handle,long start_at);
+	bool Seek(int handle, long start_at);
 	bool SeekAtStart(int handle);
-	int  Read(int handle,int buf_size,char *buf);
+	int  Read(int handle,int buf_size, char *buf);
 	void (*ListOutput)(String data);
-	void ReadFile(String pattern,String file_name);
+	void ReadFile(String pattern, String filename);
 	void (*DataOutput)(char data);
+	bool upload(String pattern, String filename, int filesize, char* filePtr);
 private:
-	long space(String pattern,unsigned char mode);	
+	long space(String pattern, unsigned char mode);	
 };
 
 #endif 
