9a442a4c59c602f0b9c529b77947b9c4ea1c1bbb
Nicholas Barnes <nicholas@nsb-electronics.co.za>
2020-01-27 14:48:50 +0200

    Add SSL_CONTEXT_ID variable
    Add new functions

--- examples/MQTT/testMQTTS/testMQTTS.ino
+++ examples/MQTT/testMQTTS/testMQTTS.ino
@@ -5,11 +5,6 @@
 #include "uc_mqtts.h"
 #include "ssl.h"
 
-UC20 gsm;
-INTERNET net;
-UCxMQTTS mqtt;
-SSL ssl;
-
 #define FORCE_REBOOT  1
 
 #define UC20_PWR_PIN  12
@@ -18,11 +13,19 @@ SSL ssl;
 #define USER ""
 #define PASS ""
 
-#define MQTT_SERVER      "io.adafruit.com"
-#define MQTT_PORT        "8883"
+#define SSL_CONTEXT_ID  1 
+
+#define MQTT_SERVER     "io.adafruit.com"
+#define MQTT_PORT       "8883"
 #define MQTT_ID          "MQTTS_TEST"   // Used to identify the connection
 #define MQTT_USER        "YOUR_USERNAME"
 #define MQTT_PASSWORD    "YOUR_PASSWORD/KEY"
+
+
+UC20 gsm;
+INTERNET net;
+UCxMQTTS mqtt;
+SSL ssl;
 
 unsigned long previousmqtt = 0;
 const long intervalmqtt = 60000;
@@ -95,36 +98,54 @@ void setup() {
   if (gsm.waitNetworkRegistered()) {
     Serial.print(F("I:\tNetwork Registered: "));
     Serial.println(gsm.getOperator());
+    delay(5000); // Give some time before getting signal quality
+    unsigned char rssi = gsm.signalQuality();
+    int dBm = gsm.signalQualitydBm(rssi);
+    unsigned char  quality = gsm.signalQualityPercentage(rssi);
     Serial.print(F("I:\tSignal Quality: "));
-    Serial.println(gsm.signalQuality());
+    Serial.print(rssi);
+    Serial.print(F("(rssi), "));
+    Serial.print(dBm);
+    Serial.print(F("(dBm), "));
+    Serial.print(quality);
+    Serial.println(F("%"));
   } else {
     Serial.print(F("E:\tNetwork Registration Issue: "));
     Serial.println(gsm.getNetworkStatus());
+    Serial.println(F("E:\tHALTING"));
+    while (1) {
+      
+    }
   }
   
   Serial.print(F("I:\tDeactivate PDP: "));
-  Serial.println(net.DisConnect());
+  Serial.println(net.disconnect());
   
   Serial.print(F("I:\tSet APN and Password: "));
-  Serial.println(net.Configure(APN, USER, PASS));
+  Serial.println(net.configure(APN, USER, PASS));
   
   Serial.print(F("I:\tConnect to Internet: "));
-  Serial.println(net.Connect());
+  Serial.println(net.connect());
+  
+  Serial.print(F("I:\tConfigure DNS: "));
+  Serial.println(net.configDNS("8.8.8.8", "9.9.9.9"));
   
   Serial.print(F("I:\tIP: "));
-  Serial.println(net.GetIP());
+  Serial.println(net.getIP());
 
   // Enable SSL
   Serial.print(F("I:\tSet SSL Version: "));
-  Serial.println(ssl.setSSLversion(1, 1));
+  Serial.println(ssl.setSSLversion(SSL_CONTEXT_ID, TLS1_2)); // Set SSL version to TLS1.2
   Serial.print(F("I:\tSet SSL Cipher: "));
-  Serial.println(ssl.setCiphersuite(1, "0xFFFF"));
+  Serial.println(ssl.setCiphersuite(SSL_CONTEXT_ID, CIPHER_ALL));  // Allow all cipher suites
   Serial.print(F("I:\tSet SSL seclevel: "));
-  Serial.println(ssl.setSeclevel(1, 0));
+  Serial.println(ssl.setSeclevel(SSL_CONTEXT_ID, SERVER_AUTH)); // Server authentication
   Serial.print(F("I:\tSet SLL Certificate: "));
-  Serial.println(ssl.setCertificate(1));
+  //Serial.println(ssl.setCertificate(SSL_CONTEXT_ID)); // Defaults to UFS:cacert.pem
+  Serial.println(ssl.setCertificate(SSL_CONTEXT_ID, "UFS:cacert.pem"));
+  Serial.print(F("I:\tSet SLL ignorelocaltime: "));
+  Serial.println(ssl.setIgnorelocaltime(SSL_CONTEXT_ID, IGNORE_TIME_CHECK)); // IGNORE_TIME_CHECK or CARE_TIME_CHECK
   
-
   feedName.concat(MQTT_USER);
   feedName.concat("/feeds/testmqttssl");
 
@@ -152,8 +173,9 @@ void callback(String topic, char *payload, unsigned char payloadLength) {
 void connectToServer() {
   do {
     Serial.print(F("I:\tConnecting to MQTT Server: "));
-    if (mqtt.disconnectMQTTServer()) {
-      mqtt.connectMQTTServer(MQTT_SERVER, MQTT_PORT);
+    if (mqtt.disconnectMQTTServer(SSL_CONTEXT_ID)) {
+      //mqtt.connectMQTTServer(MQTT_SERVER, MQTT_PORT);
+      mqtt.connectMQTTServer(SSL_CONTEXT_ID, MQTT_SERVER, MQTT_PORT);
     }
     delay(500);
     Serial.println(mqtt.connectState());
@@ -165,7 +187,7 @@ void connectToServer() {
   Serial.println(mqtt.connectCodeString(ret));
   
   mqtt.subscribe(feedName);
-  mqtt.subscribe("time/seconds"); // Adafruit IO specific
+  //mqtt.subscribe("time/seconds"); // Adafruit IO specific
 
   //Serial.println(mqtt.disconnectMQTTUser());
   //mqtt.unsubscribe("time/seconds");
